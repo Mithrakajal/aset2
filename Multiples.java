0dcd7fb266fa1128cdf6a9d12c8f5c47c1cc4b41
Mithrakajal <48437851+Mithrakajal@users.noreply.github.com>
2019-05-26 21:30:51 +0530

    Create Multiples.java

---
+++ Multiples.java
@@ -0,0 +1,14 @@
+public class Multiples
+{
+	public static void main(String[] args) {
+	    int i,n=5,l=25;
+	    System.out.println("the first 5 multiples of 5 are:");
+	    for(i=1;i<=l;i++)
+	    {
+	        if(i%n==0)
+	        {
+	            System.out.println(i+"  ");
+	        }
+	    }
+	}
+}
