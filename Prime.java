f78c4e215ede51a2fd62af9ab80c16bb0c8a47b8
Mithrakajal <48437851+Mithrakajal@users.noreply.github.com>
2019-05-26 19:14:52 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,19 @@
+public class Prime
+{
+	public static void main(String[] args) {
+		int n=11;
+		boolean flag=false;
+		for(int i=2;i<=n/2;++i)
+		{
+		    if(n%i==0)
+		    {
+		        flag=true;
+		        break;
+		    }
+		}
+		if(!flag)
+		System.out.println(n+" is a prime number");
+		else
+		System.out.println(n+" is not a prime number");
+	}
+}
